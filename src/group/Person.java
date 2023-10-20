5233b4144de23d1da38d1fd0ef98ff7acc9f5a68
Jimokunbor <2022473@student.cct.ie>
2023-10-20 18:46:09 +0100

    Make enum
    Create an Enum named "GroupNames" this is store a list of group names.

--- src/group/Person.java
+++ src/group/Person.java
@@ -10,4 +10,54 @@ package group;
  */
 public class Person {
     
+private int id;
+private String firstName;
+private String lastName;
+private String email;
+
+// Contructors to initialize person's information
+
+    public Person(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getter method for person's information
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+   // Setter method for person's information
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
