6075810face1c572671f9b7372063ab845ba8daa
Jimokunbor <2022473@student.cct.ie>
2023-10-20 17:57:32 +0100

    Create a class named "Group" to store group name and members, set constructor to initialize group name and members, Getter Setter method for group name and group members, and finally, Method to add members to the group.

--- src/group/Group.java
+++ src/group/Group.java
@@ -4,6 +4,8 @@
  */
 package group;
 
+import java.util.List;
+
 /**
  *
  * @author jimok
@@ -14,7 +16,46 @@ public class Group {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create a class named "Group" to store group name and members
+    }
+    private String name;
+    private List members;
+    
+    // set constructor to initialize group name and members
+
+    public Group(String name, List memebers) {
+        this.name = name;
+        this.members = memebers;
     }
     
+    // Getter method for group name and group members
+
+    public String getName() {
+        return name;
+    }
+
+    public List getMemebers() {
+        return members;
+    }
+    
+    // Setter method for group name and group members
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMemebers(List memebers) {
+        this.members = memebers;
+    }
+    
+    // Method to add members to the group
+    public void printGroup() {
+        System.out.println("Group Name:" + name);
+        System.out.println("Group Members: ");
+        for (Sring member : members) {
+            System.out.println(member);   
+        }
+        
+        System.out.println();
+    }
 }
